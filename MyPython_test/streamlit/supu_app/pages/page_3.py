a3d2a6c34fdc6061c6f09c20f0639348dbc04f0d
yu-syan <125536110+yu-syan@users.noreply.github.com>
2023-02-16 18:05:58 +0900

    Add files via upload

--- MyPython_test/streamlit/supu_app/pages/page_3.py
+++ MyPython_test/streamlit/supu_app/pages/page_3.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import pandas as pd
+import os
 
 # import common
+default_pass = os.path.dirname(__file__)
 
 # データ分析関連
-df = pd.read_csv("平均気温.csv", index_col="月")
+df = pd.read_csv(default_pass + "\\平均気温.csv", index_col="月")
 # st.dataframe(df)
 # st.table(df)
 st.line_chart(df)
