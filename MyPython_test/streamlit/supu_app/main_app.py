81534a6cc9d1ba89f1c8a1348605e54128db405c
yu-syan <125536110+yu-syan@users.noreply.github.com>
2023-02-16 18:31:32 +0900

    Add files via upload

--- MyPython_test/streamlit/supu_app/main_app.py
+++ MyPython_test/streamlit/supu_app/main_app.py
@@ -15,8 +15,12 @@ default_pass = os.path.dirname(__file__)
 st.title("サプーアプリ")
 st.caption("これはサプーのテストアプリです")
 
-st.text = "main_app.pyの参照先「" + default_pass + "」"
+st.subheader("自己紹介")
+st.text("main_app.pyの参照先")
+st.text(default_pass)
 
+st.text("画像参照先")
+st.text(default_pass + "\\Twitterアイコン.jpg")
 # 画像(web画面上の画像URLをそのまま設定してもOK)
 # image = Image.open(default_pass + "\\Twitterアイコン.jpg")
 image = Image.open(default_pass + "\\Twitterアイコン.jpg")
