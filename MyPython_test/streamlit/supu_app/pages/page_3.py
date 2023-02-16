0efaed6c28ac4ffe849f42812c477e16729de6f3
yu-syan <125536110+yu-syan@users.noreply.github.com>
2023-02-16 18:47:12 +0900

    Add files via upload

--- MyPython_test/streamlit/supu_app/pages/page_3.py
+++ MyPython_test/streamlit/supu_app/pages/page_3.py
@@ -6,7 +6,7 @@ import os
 default_pass = os.path.dirname(__file__)
 
 # データ分析関連
-df = pd.read_csv(default_pass + "\\平均気温.csv", index_col="月")
+df = pd.read_csv(default_pass + "\平均気温.csv", index_col="月")
 # st.dataframe(df)
 # st.table(df)
 st.line_chart(df)
