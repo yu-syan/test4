404d45d72175d250211e799b25c3f7f67102cf71
yu-syan <125536110+yu-syan@users.noreply.github.com>
2023-02-16 18:05:39 +0900

    Add files via upload

--- MyPython_test/streamlit/supu_app/main_app.py
+++ MyPython_test/streamlit/supu_app/main_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 # import common 
-# import os
+import os
 
 # # 最初に参照先を強制的に1個下の回想にする
 # try:
@@ -10,9 +10,11 @@ from PIL import Image
 # except Exception as e:
 #     pass
 
+default_pass = os.path.dirname(__file__)
+
 st.title("サプーアプリ")
 st.caption("これはサプーのテストアプリです")
 
 # 画像(web画面上の画像URLをそのまま設定してもOK)
-image = Image.open("streamlit//Twitterアイコン.jpg")
+image = Image.open(default_pass + "\\Twitterアイコン.jpg")
 st.image(image, width=200)
